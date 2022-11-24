0d54e7a896c5d1ef3ad258363e93952f819b8c89
willy836 <wilsonkimiri836@gmail.com>
2022-11-24 13:57:00 +0300

    Add homepage and complete the project

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const hamburger = document.querySelector('.hamburger');
 const links = document.querySelector('.links');
 const link = document.querySelectorAll('.link');
 
+
 hamburger.addEventListener('click', ()=> {
     hamburger.classList.toggle('active');
-    links.classList.toggle('show-links')
+    links.classList.toggle('show-links');
+    
 });
 
 link.forEach((linkItem)=> {
     linkItem.addEventListener('click', ()=> {
         hamburger.classList.remove('active');
         links.classList.remove('show-links');
+        
     })
 });
 
